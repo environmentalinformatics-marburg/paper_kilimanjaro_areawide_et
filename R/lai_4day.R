d9680738806f611988ac9536d50c9d3c5c3661b9
fdetsch <florian.detsch@staff.uni-marburg.de>
2016-06-09 16:57:24 +0200

    Initial upload: processing of 4-day LAI scenes (yet interpolation based on 1-day NDVI scenes is missing).

---
+++ R/lai_4day.R
@@ -0,0 +1,231 @@
+### environment ----------------------------------------------------------------
+
+## clear workspace
+rm(list = ls(all = TRUE))
+
+## source functions
+source("R/uniformExtent.R")
+
+## set working directory
+Orcs::setwdOS(path_lin = "/media/fdetsch/XChange/", path_win = "H:/",
+              path_ext = "kilimanjaro/evapotranspiration/")
+
+## load packages
+lib <- c("Rsenal", "MODIS", "doParallel", "kza")
+Orcs::loadPkgs(lib)
+
+## parallelization
+cl <- makeCluster(detectCores() - 1)
+registerDoParallel(cl)
+
+## modis options
+MODISoptions(localArcPath = "MODIS_ARC", outDirPath = "MODIS_ARC/PROCESSED",
+             outProj = "+init=epsg:21037")
+
+
+### data download --------------------------------------------------------------
+
+# ## combinded terra/aqua product
+# runGdal(product = "MCD15A3H", collection = "006",
+#         begin = "2013001", end = "2015365", tileH = 21, tileV = 9,
+#         SDSstring = "011100", job = "MCD15A3H.006")
+
+
+### crop layers ----------------------------------------------------------------
+
+## retrieve study extent
+ext_crp <- uniformExtent(verbose = FALSE)
+
+# ## perform crop
+# rst_crp <- foreach(i = c("Lai_500m", "Lai_QC", "Extra_QC"),
+#                    .packages = "MODIS") %dopar% {
+# 
+#     # list and import available files
+#     fls <- list.files(paste0(getOption("MODIS_outDirPath"), "/MCD15A3H.006"),
+#                       pattern = paste0(i, ".tif$"), full.names = TRUE)
+#     rst <- raster::stack(fls)
+# 
+#     # crop
+#     dir_prd <- "data/MCD15A3H.006"
+#     if (!dir.exists(dir_prd)) dir.create(dir_prd)
+#     
+#     dir_out <- paste0(dir_prd, "/crp")
+#     if (!dir.exists(dir_out)) dir.create(dir_out)
+# 
+#     fls_out <- paste(dir_out, basename(fls), sep = "/")
+#     rst_out <- raster::crop(rst, ext_crp, snap = "out")
+# 
+#     # convert to 8-bit unsigned integer
+#     raster::dataType(rst_out) <- "INT1U"
+# 
+#     # apply scale factor
+#     if (i == "Lai_500m")
+#       rst_out <- rst_out * 0.1
+# 
+#     # save and return cropped layers
+#     lst_out <- lapply(1:nlayers(rst_out), function(j)
+#       raster::writeRaster(rst_out[[j]], filename = fls_out[j],
+#                           format = "GTiff", overwrite = TRUE)
+#     )
+# 
+#     raster::stack(lst_out)
+#   }
+
+## reimport cropped files
+rst_crp <- foreach(i = c("Lai_500m", "Lai_QC", "Extra_QC"), 
+                   .packages = "raster") %dopar% {
+
+  # list and import available files
+  dir_crp <- "data/MCD15A3H.006/crp"
+  fls_crp <- list.files(dir_crp, pattern = paste0(i, ".tif$"), full.names = TRUE)
+  raster::stack(fls_crp)
+}
+
+
+### quality control, step #1: --------------------------------------------------
+### discard cloudy pixels based on companion quality information ('Lai_QC')
+
+dir_qc1 <- "data/MCD15A3H.006/qc1"
+if (!dir.exists(dir_qc1)) dir.create(dir_qc1)
+
+# ## perform quality check #1
+# lst_qc1 <- foreach(i = 1:nlayers(rst_crp[[1]]), .packages = lib) %dopar% {
+#     overlay(rst_crp[[1]][[i]], rst_crp[[2]][[i]], fun = function(x, y) {
+#       id <- sapply(y[], function(k) {
+#         bin <- number2binary(k, 8, TRUE)
+#         modland <- substr(bin, 8, 8) == "0"
+#         cloud_state <- substr(bin, 4, 5) == "00"
+#         confidence <- substr(bin, 1, 3) %in% c("000", "001")
+# 
+#         all(modland, cloud_state, confidence)
+#       })
+# 
+#       x[!id] <- NA
+#       return(x)
+#     }, filename = paste(dir_qc1, names(rst_crp[[1]][[i]]), sep = "/"),
+#     overwrite = TRUE, format = "GTiff")
+# }
+# 
+# rst_qc1 <- stack(lst_qc1)
+
+## reimport step #1 quality-controlled files
+fls_qc1 <- list.files(dir_qc1, pattern = "Lai_500m.tif$", full.names = TRUE)
+rst_qc1 <- raster::stack(fls_qc1)
+
+
+### quality control, step #2: --------------------------------------------------
+### discard cloudy pixels based on 'state_250m' flags
+
+dir_qc2 <- "data/MCD15A3H.006/qc2"
+if (!dir.exists(dir_qc2)) dir.create(dir_qc2)
+
+# ## perform quality check #2
+# lst_qc2 <- foreach(i = 1:nlayers(rst_qc1), .packages = lib) %dopar% {
+#     overlay(rst_qc1[[i]], rst_crp[[2]][[i]], fun = function(x, y) {
+#       id <- sapply(y[], function(k) {
+#         bin <- number2binary(k, 8, TRUE)
+#         snow <- substr(bin, 6, 6) == "0"
+#         cirrus <- substr(bin, 4, 4) == "0"
+#         intern_cloud <- substr(bin, 3, 3) == "0"
+#         cloud_shadow <- substr(bin, 2, 2) == "0"
+# 
+#         all(snow, cirrus, intern_cloud, cloud_shadow)
+#       })
+# 
+#       x[!id] <- NA
+#       return(x)
+#     }, filename = paste(dir_qc2, names(rst_qc1[[i]]), sep = "/"),
+#     overwrite = TRUE, format = "GTiff")
+# }
+# 
+# rst_qc2 <- raster::stack(lst_qc2)
+
+## reimport step #2 quality-controlled files
+fls_qc2 <- list.files(dir_qc2, pattern = "Lai_500m.tif$", full.names = TRUE)
+rst_qc2 <- raster::stack(fls_qc2)
+
+
+### quality control, step #3: --------------------------------------------------
+### discard pixels adjacent to clouds
+
+dir_adj <- "data/MCD15A3H.006/adj"
+if (!dir.exists(dir_adj)) dir.create(dir_adj)
+
+# lst_adj <- foreach(i = unstack(rst_qc2), .packages = "raster") %dopar% {
+#   msk <- raster::focal(i, w = matrix(c(1, 1, 1, 1, 0, 1, 1, 1, 1), 3, 3), 
+#                        fun = function(...) any(is.na(...)))
+#   
+#   raster::overlay(i, msk, fun = function(x, y) {
+#     x[y[] == 1] <- NA
+#     return(x)
+#   }, filename = paste(dir_adj, names(i), sep = "/"), 
+#   format = "GTiff", overwrite = TRUE)
+# }
+# 
+# rst_adj <- stack(lst_adj)
+
+## reimport step #3 quality-controlled files
+fls_qc3 <- list.files(dir_qc3, pattern = "Lai_500m.tif$", full.names = TRUE)
+rst_qc3 <- raster::stack(fls_qc3)
+
+
+### gap-filling ----------------------------------------------------------------
+### kolmogorov-zurbenko adaptive
+
+mat_adj <- as.matrix(rst_adj)
+
+mat_gf <- t(apply(mat_adj, 1, FUN = function(x) kza(x, m = 5)$kz))
+rst_gf <- setValues(rst_adj, mat_gf)
+
+dir_gf <- "data/MCD15A3H.006/gf"
+if (!dir.exists(dir_gf)) dir.create(dir_gf)
+
+lst_gf <- foreach(i = unstack(rst_gf), j = unstack(rst_adj), 
+                  .packages = "raster") %dopar% 
+  raster::writeRaster(i, filename = paste(dir_gf, names(j), sep = "/"), 
+                      format = "GTiff", overwrite = TRUE)
+
+rst_gf <- stack(lst_gf)
+
+
+### replicate missing dates -----
+
+dts <- seq(as.Date("2013-01-01"), as.Date("2015-12-31"), 1)
+dts <- strftime(dts, format = "%Y%j")
+
+dts_gf <- extractDate(names(rst_gf), 11, 17)$inputLayerDates
+
+dat_rpl <- merge(data.frame(date = dts), 
+                 data.frame(date = dts_gf, file = names(rst_gf)), 
+                 by = "date", all.x = TRUE)
+
+## replicate available files
+for (i in 1:nrow(dat_rpl)) {
+  if (is.na(dat_rpl$file[i])) {
+    dat_rpl$file[i] <- dat_rpl$file[i-1]
+  }
+}
+
+## target folder and files
+dir_rpl <- "data/MCD15A3H.006/rpl"
+if (!dir.exists(dir_rpl)) dir.create(dir_rpl)
+
+fls_rpl <- paste0(dir_gf, "/", dat_rpl$file, ".tif")
+rst_rpl <- stack(fls_rpl)
+
+fls_rpl_out <- sapply(1:nrow(dat_rpl), function(i) {
+  gsub(extractDate(as.character(dat_rpl$file[i]), 11, 17)$inputLayerDates, 
+       as.character(dat_rpl$date[i]), as.character(dat_rpl$file[i]))
+})
+fls_rpl_out <- paste0(dir_rpl, "/", fls_rpl_out, ".tif")
+
+## write to files
+lst_rpl <- foreach(i = 1:nlayers(rst_rpl), j = fls_rpl_out, 
+                   .packages = "raster") %dopar% {
+  writeRaster(rst_rpl[[i]], filename = j, format = "GTiff", overwrite = TRUE)
+}
+
+rst_rpl <- stack(lst_rpl)
+
+## close parallel backend
+stopCluster(cl)
